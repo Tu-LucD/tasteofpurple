557e8910a0494c1eb50620bca41ecae7b6af8c25
Tu-LucD <tlduong123@gmail.com>
2022-04-10 15:51:11 -0400

    Overhaul

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/core/styles"
+
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from "react-router-dom";
+
+import {
+  Drawer, List, ListItem,
+  ListItemIcon, ListItemText,
+  Container, Typography,
+} from "@material-ui/core";
+
+import HomeIcon from "@material-ui/icons/Home";
+import InfoIcon from '@material-ui/icons/Info';
+
+//Pages
+import Welcome from "./Pages/Welcome"
+
+const useStyles = makeStyles((theme) => ({
+  drawerPaper: { width: 'inherit' },
+  link: {
+    textDecoration: 'none',
+    color: theme.palette.text.primary
+  }
+}))
+
+function App() {
+  const classes = useStyles();
+  return (
+    <Router>
+      <div style={{ display: 'flex' }}>
+        <Drawer
+          style={{ width: '220px' }}
+          variant="persistent"
+          anchor="left"
+          open={true}
+          classes={{ paper: classes.drawerPaper }}
+        >
+          <List>
+            <Link to="/" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
+                <ListItemText primary={"Home"} />
+              </ListItem>
+            </Link>
+            <Link to="/about" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <InfoIcon />
+                </ListItemIcon>
+                <ListItemText primary={"About"} />
+              </ListItem>
+            </Link>
+          </List>
+        </Drawer>
+        <Switch>
+          <Route exact path="/">
+            <Welcome/>
+          </Route>
+          <Route exact path="/about">
+            <Container>
+              <Typography variant="h3" gutterBottom>
+                About
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+              </Typography>
+            </Container>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
